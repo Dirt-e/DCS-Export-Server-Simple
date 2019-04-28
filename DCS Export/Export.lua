dc66ba890637c9f7021fa858ae368a80894d2e75
Dirt-e <Dirt-e@users.noreply.github.com>
2019-04-28 11:55:52 +0200

    My first publishing on GitHub

---
+++ DCS Export/Export.lua
@@ -0,0 +1,37 @@
+
+function LuaExportStart()
+
+	package.path  = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua"
+	package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
+
+	socket = require("socket")
+	IPAddress = "127.0.0.1"
+	Port = 31090
+
+	MySocket = socket.try(socket.connect(IPAddress, Port))
+	MySocket:setoption("tcp-nodelay",true) 
+	
+end
+
+function LuaExportBeforeNextFrame()
+end
+
+function LuaExportAfterNextFrame()
+
+	local IAS = LoGetIndicatedAirSpeed()
+	
+	socket.try(MySocket:send(string.format("IAS: %.4f \n",IAS)))
+	
+end
+
+function LuaExportStop()
+
+	if MySocket then 
+		socket.try(MySocket:send("exit"))
+		MySocket:close()
+	end
+end
+
+function LuaExportActivityNextEvent(t)
+
+end
---
+++ TCP-Server Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Server_Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 31090);
+            listener.Start();
+
+            Console.WriteLine("Server started.");
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for DCS connection...");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("DCS connected :-)");
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+
+                string s = string.Empty;
+
+                while (true)
+                {
+                    s = reader.ReadLine();
+                    Console.WriteLine(s);
+                    if (s == "exit") break;
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }
+        } //End Main
+    }//End Class
+}
