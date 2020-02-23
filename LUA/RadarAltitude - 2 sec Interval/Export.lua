9c02f6aadcfc3647d7c562513891faed2203f3b6
Dirt-e <Dirt-e@users.noreply.github.com>
2020-02-23 09:54:30 +0100

    Added example for export data at interval

---
+++ LUA/RadarAltitude - 2 sec Interval/Export.lua
@@ -0,0 +1,39 @@
+
+function LuaExportStart()
+
+	package.path  = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua"
+	package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
+
+	socket = require("socket")
+	IPAddress = "127.0.0.1"
+	Port = 31090
+
+	MySocket = socket.try(socket.connect(IPAddress, Port))
+	MySocket:setoption("tcp-nodelay",true) 
+end
+
+function LuaExportBeforeNextFrame()
+end
+
+function LuaExportAfterNextFrame()
+end
+
+function LuaExportStop()
+
+	if MySocket then 
+		socket.try(MySocket:send("exit"))
+		MySocket:close()
+	end
+end
+
+function LuaExportActivityNextEvent(t)
+
+	local tNext = t 
+
+	local RALT = LoGetAltitudeAboveGroundLevel()
+	
+	socket.try(MySocket:send(string.format("RadAlt [m]: %.4f \n",RALT)))
+
+	tNext = tNext + 2.0
+	return tNext
+end
